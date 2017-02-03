9d11930df556beb8283652e72f70328aa11b9013
KuoYu-chiung <genekuo@cybersoft4u.com>
2017-02-03 15:09:03 +0800

    add tempconv and use it

--- example/hello.go
+++ example/hello.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
-	"github.com/genekuo/hello/stringutil"
+	"github.com/genekuo/hello/tempconv"
 )
 
 func main() {
-	fmt.Printf(stringutil.Reverse("!oG ,olleH"))
+	//fmt.Printf(stringutil.Reverse("!oG ,olleH"))
+	//fmt.Printf("Brrrrr! %v\n", tempconv.AbsoluteZeroC)
+	fmt.Println(tempconv.CToF(tempconv.BoilingC))
 }
